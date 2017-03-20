fc4ebb73bf278ec6e734f32e222dbcf1b0cf7800
Heorhi Vysotski <heorhi_vysotski@epam.com>
2017-03-20 21:58:22 +0300

    Delete Task2.4_HVysotski.py

--- Task2.4_HVysotski.py
+++
@@ -1,9 +0,0 @@
-import re
-from collections import Counter
-
-
-file = open('access_log')
-data = re.findall('[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}', file.read())
-file.close()
-for index in Counter(data).most_common(10):
-    print("IP Address - %s, Count - %d" % (index[0], index[1]))
