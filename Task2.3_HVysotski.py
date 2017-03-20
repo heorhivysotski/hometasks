dc419711cd4f4b0a572fc19763c660b82b6ed866
Heorhi Vysotski <heorhi_vysotski@epam.com>
2017-03-20 21:58:12 +0300

    Delete Task2.3_HVysotski.py

--- Task2.3_HVysotski.py
+++
@@ -1,7 +0,0 @@
-a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
-b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
-
-def CommonList(first_list, second_list):
-    print(list(set(first_list) & set(second_list)))
-
-CommonList(a, b)
